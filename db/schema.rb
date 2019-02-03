d55da7643f675c76ff0cbba7beacc801fa7030a9
Vinender Singh <vinendersingh91@gmail.com>
2019-02-03 21:38:55 +0530

    create articles tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_02_03_153557) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.time "created_at"
+    t.time "updated_at"
+  end
 
 end
