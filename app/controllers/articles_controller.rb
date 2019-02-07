cf17ae34ec0a380814c0d1d7c7126eb7bfac508e
Vinender Singh <vinendersingh91@gmail.com>
2019-02-07 16:16:21 +0530

    Fix create, add flash messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,9 +8,17 @@ class ArticlesController < ApplicationController
 		#render plain: params[:articles].inspect
 		#This will display what we are passing in
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to_articles_show(@article)
+		if @article.save
+		 flash[:notice]= "Article was successfully created"
+		 redirect_to article_path(@article)
+	else 
+		render 'new'
+	end 
 	end
+
+	def show
+		@article = Article.find(params[:id])
+	end 
 
 	private 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-root 'pages#home'
+root 'pages#home'   
 get 'about', to: 'pages#about'
 resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
