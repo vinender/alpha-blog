763372166150aaddb7dee553507b0877653b308f
Vinender Singh <vinendersingh91@gmail.com>
2019-02-09 15:50:52 +0530

    Abstract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+   before_action :set_article, only: [:edit, :update, :show, :destroy]
+	
+
 	def index
 		@articles = Article.all 
 	end 
@@ -8,7 +12,6 @@ class ArticlesController < ApplicationController
 	end 
 
 	def edit 
-	@article = Article.find(params[:id]) 
 	end 
 
 	def create
@@ -25,7 +28,6 @@ end
 	
 
 	def update
-		@article = Article.find(params[:id])
 		if @article.update(article_params)
 			flash[:notice] = "Article was successfully updated"
 			redirect_to article_path(@article)
@@ -37,17 +39,19 @@ end
 
 
 	def show
-		@article = Article.find(params[:id])
 	end 
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
 		flash[:notice]= "Article was successfully deleted"	
 		redirect_to articles_path
 	end 
 
 	private 
+
+	def set_article
+		@article = Article.find(params[:id])
+	end 
 
 	def article_params
 
