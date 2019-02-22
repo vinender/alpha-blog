42632e22738a39f81ba5e730b6beee4c85d82a19
Vinender Singh <vinendersingh91@gmail.com>
2019-02-22 14:52:11 +0530

    create uers tables and models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_153557) do
+ActiveRecord::Schema.define(version: 2019_02_22_091456) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.time "created_at"
     t.time "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
