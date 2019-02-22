d21f736599a61d472ab271489887ad9957089aa1
Vinender Singh <vinendersingh91@gmail.com>
2019-02-22 23:05:38 +0530

    Add secure password and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,5 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, length: { minimum: 12, maximum: 105},
 	uniqueness: {case_sensitive: false},
 	format: {with: VALID_EMAIL_REGEX}
-
-	
+	has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_22_123922) do
+ActiveRecord::Schema.define(version: 2019_02_22_165845) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_02_22_123922) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
