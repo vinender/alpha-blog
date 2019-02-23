1a8ab5d98926b485b6e2a5bafd2f41e1aa70eb34
Vinender Singh <vinendersingh91@gmail.com>
2019-02-23 19:21:07 +0530

    Create show action and add gravatar image

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ get 'about', to: 'pages#about'
 resources :articles
 
 get 'signup', to: 'users#new'
-# post 'users', to: 'users#create' 
+# post 'users', to: 'users#create' or
 resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end  
