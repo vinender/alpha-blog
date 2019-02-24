8ee8bd941c95175e962fe39c1c92903ccb8dfa81
Vinender Singh <vinendersingh91@gmail.com>
2019-02-24 22:42:19 +0530

    Complete authentication

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ resources :articles
 get 'signup', to: 'users#new'
 # post 'users', to: 'users#create' or
 resources :users, except: [:new]
+
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end  
