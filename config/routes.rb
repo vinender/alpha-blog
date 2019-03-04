a24416d96881450e18d6f35e7034245d3ace214b
Vinender Singh <vinendersingh91@gmail.com>
2019-03-04 16:12:27 +0530

    Add categories and models

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ resources :users, except: [:new]
 get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
+resources :categories, except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end  
