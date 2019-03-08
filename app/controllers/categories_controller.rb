7f78b4018de103deb30f64f0e5b9b738957336be
Vinender Singh <vinendersingh91@gmail.com>
2019-03-08 17:29:49 +0530

    Update all views and categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -33,7 +33,8 @@ end
 end
 
 def show
-
+	@category = Category.find(params[:id])
+	@category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
 end
 
 private
@@ -57,4 +58,3 @@ end
 end
 
 end
-
